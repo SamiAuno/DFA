9830931019b797563369f6cad8ab1165e71d871d
Sami Auno <sami.auno@gmail.com>
2020-03-30 15:39:36 +0300

    Streamlined, removed old code.

--- routine_fluctuation.m
+++ routine_fluctuation.m
@@ -27,12 +27,6 @@ function [F,F_var] = routine_fluctuation(DATA, n, N_data, N_chan, avgFlag)
         detrended_DATA_std(:,di) = detrended_DATA_segments_std(((di-1)*N_chan+1):(di*N_chan), 1);
     end
     
-    % three sigma exclusion threshold
-    % take the standard deviation of detrended_DATA_std over all channels
-    % and exclude all values that are over 3 sigmas away from the median
-%     F_std = std(detrended_DATA_std(:));
-%     Fmedian = median(detrended_DATA_std(:));
-%     exclusion_threshold = [Fmedian - 3*F_std, Fmedian + 3*F_std];
     
     F_var = var(detrended_DATA_std,0,2);
     
